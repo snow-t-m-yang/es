a1d7532612584a0b8c2d838e4fd64a448a4ca19c
Snow Yang <snow.yang@fluv.com>
2023-08-05 22:02:00 +0800

    Finish Landing page layout

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -4,15 +4,15 @@ import Image from 'next/image';
 const Landing = () => {
   return (
     <div className="relative flex flex-col items-center justify-center min-w-full min-h-screen overflow-x-hidden">
-      <div className="absolute z-10 flex flex-col justify-center h-full">
+      <div className="absolute z-10 grid h-full grid-rows-3 py-7">
         <Image
-          className=""
+          className="row-start-2 "
           src="dark-logo.svg"
           width={300}
           height={300}
           alt="Eternal Story Logo"
         />
-        <h1 className="text-3xl font-bold">
+        <h1 className="self-end row-start-3 text-4xl font-bold">
           ONLY
           <br />
           STORIES ARE
