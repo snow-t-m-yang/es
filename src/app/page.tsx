75ebfd00abdc521b787e5c2ebf112eee896635f7
Snow Yang <snow.yang@fluv.com>
2023-08-05 15:16:47 +0800

    Add Landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
-import Image from 'next/image'
+import Landing from '@/components/Landing'
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between min-h-screen p-24">
-      <Image src={'/logo1.png'} alt='logo' width={100} height={100}/>
-      <h1 className="z-10 text-4xl font-bold text-center capitalize">
-        Eternal Story .LTD
-      </h1>
-      <Image src={'/background.png'} fill alt='log'/>
+      <Landing />
     </main>
   )
 }
+ 
