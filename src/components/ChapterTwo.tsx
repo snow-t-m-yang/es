aa99d5b4236d4ea4196364b0ca88ff55dfae723d
Rhaenyra Liang <rhaenyraliang1999@gmail.com>
2023-08-27 17:32:12 +0800

    Add banner

--- src/components/ChapterTwo.tsx
+++ src/components/ChapterTwo.tsx
@@ -1,10 +1,12 @@
+import Image from 'next/image';
+
 function ChapterTwo() {
   return (
     <div
       id="Product"
-      className="flex flex-col items-center h-full gap-8 pt-20 bg-black"
+      className="flex flex-col items-center h-full gap-8 bg-black"
     >
-      <div className="flex flex-col items-center w-full gap-4">
+      <div className="flex flex-col items-center justify-center w-full h-32 gap-4 bg-cover bg-banner">
         <h1 className="text-3xl font-bold">CHAPTER TWO</h1>
         <h2 className="text-lg font-medium">Our products</h2>
       </div>
