9d6a3a6e464700c39b548e91cca05215d5f764ff
Rhaenyra Liang <rhaenyraliang1999@gmail.com>
2023-08-29 15:31:48 +0800

    Execute pnpm add embla-carousel-react autoplay

--- src/components/ChapterThree.tsx
+++ src/components/ChapterThree.tsx
@@ -1,4 +1,7 @@
-import 'lightgallery/css/lightgallery.css';
+'use client';
+import useEmblaCarousel, { EmblaOptionsType } from 'embla-carousel-react';
+import Image from 'next/image';
+import { useEffect } from 'react';
 
 function ChapterThree() {
   type ChapterThreeImageType = {
@@ -7,7 +10,7 @@ function ChapterThree() {
     id: string;
   };
 
-  const ChapterThreeImageItems: ChapterThreeImageType[] = [
+  const chapterThreeImageItems: ChapterThreeImageType[] = [
     {
       imgSrc: '/cp3/1.jpg',
       alt: 'chapter3 photo',
@@ -30,11 +33,44 @@ function ChapterThree() {
     },
   ];
 
+  type CarouselProps = {
+    options?: EmblaOptionsType;
+    children: React.ReactNode;
+  };
+
+  // const [emblaRef] = useEmblaCarousel({
+  //   slidesToScroll: 1,
+  //   align: 'start',
+  // });
+
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: false });
+
+  useEffect(() => {
+    if (emblaApi) {
+      console.log(emblaApi.slideNodes()); // Access API
+    }
+  }, [emblaApi]);
+
   return (
     <div id="Contact" className="flex flex-col items-center h-full bg-black">
       <div className="flex flex-col items-center justify-center w-[97%] h-28 gap-4 bg-cover bg-banner">
         <h1 className="text-3xl font-bold">CHAPTER THREE</h1>
         <h2 className="text-lg font-medium">Contact us</h2>
+      </div>
+      <div className="flex" ref={emblaRef}>
+        {chapterThreeImageItems.map((items) => {
+          return (
+            <div>
+              <Image
+                key={items.id}
+                src={items.imgSrc}
+                alt={items.alt}
+                width={100}
+                height={100}
+              />
+            </div>
+          );
+        })}
       </div>
     </div>
   );
