3c1aeacf820c01ab3058c91181c249e63429ed5b
Snow Yang <snow.yang@fluv.com>
2023-09-30 10:30:58 +0800

    Fix landing logo overflow under nav

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="scroll-smooth">
-      <body className={`${inter.className} w-screen overflow-hidden h-screen`}>
+      <body className={`${inter.className} w-screen overflow-hidden h-[100dvh]`}>
         <Navbar />
         {children}
       </body>
