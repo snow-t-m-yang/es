c5ae649e97a6c45f34d72001ba0094b5dccabd91
Snow Yang <snow.yang@fluv.com>
2023-11-25 21:56:58 +0800

    Update Intro and ModeToggle components

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -4,7 +4,7 @@ const Intro = () => {
   
   const t = useTranslations("Intro")
   return (
-    <div className="py-20 max-w-2xl mx-auto space-y-5">
+    <div className="py-20 px-7 md:px-0 transition-all duration-200 max-w-2xl mx-auto space-y-5">
       <h2 className="text-2xl font-bold">{t('title')}</h2>
       <p>
         {t('description')}
