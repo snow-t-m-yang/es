85b887489b4c6da4353812327d5ea52625e6af4d
Snow Yang <snow.yang@fluv.com>
2024-03-05 19:17:51 +0800

    Refactor ImageWithOverlay component to improve readability and layout

--- src/components/carousel/image-with-overlay.tsx
+++ src/components/carousel/image-with-overlay.tsx
@@ -32,7 +32,7 @@ export function ImageWithOverlay({
   return (
     <div className="relative w-full h-full" onClick={handleClick}>
       <Image
-        className="object-cover w-full h-auto rounded-md"
+        className="object-cover rounded-md"
         fill
         src={imgSrc}
         alt={alt}
@@ -42,27 +42,25 @@ export function ImageWithOverlay({
         <div className={``}>
           <div className="absolute inset-0 bg-black rounded-md opacity-80"></div>
           <div className="absolute inset-0 h-full backdrop-blur-md">
-            <div className="flex flex-col relative justify-center h-full gap-y-16 text-center">
-              <div className="space-y-7 px-20">
-                <h1 className="text-xl font-semibold text-white sm:text-3xl">
-                  {title}
-                </h1>
-                <p className="text-sm text-gray-200">{description}</p>
+            <div className="relative flex flex-col justify-center h-full text-center gap-y-16">
+              <div className="flex flex-col px-3 py-5 gap-y-5 md:px-20">
+                <div className="gap-y-5">
+                  <h1 className="text-xl font-semibold text-white sm:text-3xl">
+                    {title}
+                  </h1>
+                  <p className="text-sm text-gray-200">{description}</p>
+                </div>
+                {link && icon && (
+                  <Link className="self-end" href={link} target="_blank">
+                    <Image
+                      src={youtube}
+                      alt="instagram logo"
+                      height={100}
+                      width={100}
+                    />
+                  </Link>
+                )}
               </div>
-              {link && icon && (
-                <Link
-                  className="absolute bottom-12 right-12"
-                  href={link}
-                  target="_blank"
-                >
-                  <Image
-                    src={youtube}
-                    alt="instagram logo"
-                    height={100}
-                    width={100}
-                  />
-                </Link>
-              )}
             </div>
           </div>
         </div>
