f278555965104e56d03a047203bb9ff91a5bfbbe
Snow Yang <snow.yang@fluv.com>
2024-03-06 12:54:40 +0800

    Add partner images

---
+++ public/partners/index.ts
@@ -0,0 +1,6 @@
+export { default as partners1 } from "./partners-1.png";
+export { default as partners2 } from "./partners-2.png";
+export { default as partners3 } from "./partners-3-milktea-and-friends.png";
+export { default as partners4 } from "./partners-4.png";
+export { default as partners5 } from "./partners-5.png";
+export { default as partners6 } from "./partners-6.png";
